116708d67d94b71f7f2e1bd20211f107fcfcaec6
PetarKostadinov <petarkostadinov30@gmail.com>
2023-03-03 14:33:16 +0200

    Adding footer and bootstrap css

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,34 @@
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Product from "./components/Product";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import LinkContainer from "react-router-bootstrap/LinkContainer";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">shoping well</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>shoping well</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/product/:slug" element={<Product />}></Route>
-          </Routes>
-
+          <Container>
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/product/:slug" element={<Product />}></Route>
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All rights reserved. Code: Petar Kostadinov</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
