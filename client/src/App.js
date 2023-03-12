9a1d31e3e4be6c54f364f345517715918a0bb89b
PetarKostadinov <petarkostadinov30@gmail.com>
2023-03-12 22:26:53 +0200

    Side bar and Search input implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
-import { ToastContainer } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import LinkContainer from "react-router-bootstrap/LinkContainer";
-import { Badge, Nav, NavDropdown } from "react-bootstrap";
-import { useContext } from "react";
+import { Badge, Button, Nav, NavDropdown } from "react-bootstrap";
+import { useContext, useEffect, useState } from "react";
 import { Store } from "./components/Store";
 import CartScreen from "./components/CartScreen";
 import ProductScreen from "./components/ProductScreen";
@@ -19,6 +19,9 @@ import Order from "./components/Order";
 import OrderSummary from "./components/OrderSummary";
 import OrderHistory from "./components/OrderHistory";
 import Profile from "./components/Profile";
+import getError from "./util";
+import axios from "axios";
+import Search from "./components/Search";
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -31,15 +34,39 @@ function App() {
     localStorage.removeItem('shippingInfo');
     localStorage.removeItem('paymentMethod');
     window.location.href = '/login';
-  }
+  };
+
+  const [sideBarIsOpen, setSideBarIsOpen] = useState(false);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const { data } = await axios.get(`/api/products/categories`);
+        setCategories(data);
+      } catch (err) {
+        toast.error(getError(err));
+      }
+    };
+    fetchCategories();
+  }, [])
 
   return (
     <BrowserRouter>
-      <div className="d-flex flex-column site-container">
+      <div className={sideBarIsOpen ?
+        "d-flex flex-column site-container active-cont"
+        : "d-flex flex-column site=container"
+      }>
         <ToastContainer position="bottom-center" limit={1}></ToastContainer>
         <header>
           <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
+              <Button
+                variant="dark"
+                onClick={() => setSideBarIsOpen(!sideBarIsOpen)}
+              >
+                <i className="fas fa-bars"></i>
+              </Button>
               <LinkContainer to="/">
                 <Navbar.Brand>
                   <img src="/images/linkedin_banner_image_2.png" alt="shopping well" />
@@ -47,6 +74,7 @@ function App() {
               </LinkContainer>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
+                <Search />
                 <Nav className="me-auto w-100 justify-content-end" >
                   <Link to="/cart" className="nav-link">
                     Cart
@@ -83,6 +111,27 @@ function App() {
             </Container>
           </Navbar>
         </header>
+        <div
+          className={
+            sideBarIsOpen ?
+              'active-nav side-navbar d-flex justify-content-between flex-wrap flex-column'
+              : 'side-navbar d-flex justify-content-between flex-wrap flex-column'
+          }
+        >
+          <Nav className="flex-column text-white w-100 p-2">
+            <Nav.Item>
+              <strong>Categories</strong>
+            </Nav.Item>
+            {categories.map((category) => (
+              <Nav.Item key={category}>
+                <LinkContainer to={`/search?category=${category}`} onClick={() => setSideBarIsOpen(false)}>
+                  <Nav.Link>{category}</Nav.Link>
+                </LinkContainer>
+              </Nav.Item>
+            ))}
+          </Nav>
+
+        </div>
         <main>
           <Container className="mt-3">
             <Routes>
