e264a3daa89657435b44a3c3adc7da36ac3d705c
PetarKostadinov <petarkostadinov30@gmail.com>
2023-07-31 23:54:20 +0300

    carousel change

--- client/src/helpersComponents/Carousel.js
+++ client/src/helpersComponents/Carousel.js
@@ -16,9 +16,7 @@ function CarouselComponent() {
     <Carousel.Item>
       <Image className="d-block w-100" src="https://vida.bg/wp-content/uploads/2021/09/kraken-category-header.png" alt="Home Page" />
     </Carousel.Item>
-    <Carousel.Item>
-      <Image className="d-block w-100" src="https://cdn-ccnlk.nitrocdn.com/FagQESmbWOQjOsPKkgdiNUYIEZTBiuRg/assets/images/optimized/rev-56322ca/wp-content/uploads/2022/10/Beluga-Category-Banner-1920x314-V1-1920x314.jpg" alt="Home Page" />
-    </Carousel.Item>
+
   </Carousel>
   )
 }
